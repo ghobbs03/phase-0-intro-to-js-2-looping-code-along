bc2f6f8a4aaf788deb0c69e1a8c1f6a4f4c5f4b8
Gail Hobbs <gbhobbs97@gmail.com>
2023-01-31 09:57:18 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    const cards = [];
+
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+
+    return cards;
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i)
+    }
+}
